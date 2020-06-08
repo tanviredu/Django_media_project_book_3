e4cca3cce4ba2f58c1f9338cc1b01478947cb5b3
tanviredu <ornobtanvir.git@gmail.com>
2020-06-08 20:17:46 +0600

    added

--- mysite/article/models.py
+++ mysite/article/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from time import time
 
-# Create your models here.
+
+def get_upload_file_name(instance,filename):
+    return "uploaded_files/%s_%s"%(str(time()).replace('.','_'),filename)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length =200)
+    body = models.TextField()
+    pub_date = models.DateTimeField('Published Date')
+    likes = models.IntegerField()
+    thumbnail = models.FileField(upload_to=get_upload_file_name)
+
+
+
+    def __str__(self):
+
+        return str(self.title)
