75a13a49872ecb2b70f452bb9fadac525ac0078c
tanviredu <ornobtanvir.git@gmail.com>
2020-06-08 20:25:16 +0600

    added

--- mysite/article/admin.py
+++ mysite/article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
+#admin.site.register(Article)
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_date','likes',)
+    list_filter = ('title','pub_date','likes',)
+    search_fields = ('title','body','pub_date','likes',)
